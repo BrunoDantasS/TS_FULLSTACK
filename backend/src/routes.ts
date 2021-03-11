bd9204da1ba7264a610b0405552ce3bb02e1f96f
Bruno Wesley <bruno.wesley77@gmail.com>
2021-03-11 13:15:58 -0300

    update routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,6 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
 import { deleteAdress, getAdress, getAdressById, postAdress, putAdress } from "./controller/AdressController";
-import { getUsers, postUser, getUserById, putUser, deleteUser, login } from "./controller/UserController";
+import { getUsers, postUser, putUser, deleteUser, login, getUserById } from "./controller/UserController";
 import { auth } from "./middleware/auth"
 
 
@@ -9,23 +9,23 @@ const routes = Router();
 routes.get('/', (req, res) => {
     res.json({
         message: 'Hello World'
-    });
+    })
 });
 
-routes.post('/login', login)
-routes.post('/register', postUser);
+routes.post('/login', login);
 
 routes.use(auth)
 
-routes.get('/user', getUsers);
-routes.get('/user/:id', getUserById);
-routes.put('/user/:id', putUser);
-routes.delete('/user/:id', deleteUser);
+routes.post('/admin/post-user', postUser);
+routes.get('/admin/get-user', getUsers);
+routes.get('/admin/get-user/:id', getUserById);
+routes.put('/admin/put-user/:id', putUser);
+routes.delete('/admin/delete-user/:id', deleteUser);
 
-routes.post('/adress', postAdress);
-routes.get('/adress', getAdress);
-routes.get('/adress/:id', getAdressById);
-routes.put('/adress/:id', putAdress);
-routes.delete('/adress/:id', deleteAdress);
+routes.post('/admin/post-adress', postAdress);
+routes.get('/admin/get-adress', getAdress);
+routes.get('/admin/get-adress/:id', getAdressById);
+routes.put('/admin/put-adress/:id', putAdress);
+routes.delete('admin/delete-adress/:id', deleteAdress);
 
 export default routes;
